ef3f41169b713f05cbe833140fffd6b19a586e7d
Vadim Denisovski <48020188+Vadzim1989@users.noreply.github.com>
2024-11-13 21:30:57 +0300

    min-height

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -9,7 +9,7 @@
       @on-click-favorite="() => emit('addToFavorite', modalData)"
     />
   </teleport>
-  <div class="flex justify-evenly flex-row flex-wrap gap-4" v-auto-animate>
+  <div class="flex justify-evenly flex-row flex-wrap gap-4 card-list" v-auto-animate>
     <Card
       v-for="item in items"
       :key="item.id"
@@ -40,3 +40,8 @@ function showModal(obj) {
   modalData.value = obj;
 }
 </script>
+<style scoped>
+.card-list {
+  min-height: 55vh;
+}
+</style>
