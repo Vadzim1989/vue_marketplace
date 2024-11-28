0f4ff4a4a234049f3ad3de77221cae71fc769cc9
Vadim Denisovski <48020188+Vadzim1989@users.noreply.github.com>
2024-11-28 20:14:25 +0300

    loading

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,15 +1,16 @@
 <template>
-    <form @submit.stop.prevent class="auth-form">
+    <Loader v-if="loading" />
+    <form v-if="!loading" @submit.stop.prevent class="auth-form">
         <div class="inputs-form">
             <input class="focus:outline-yellow-500 focus:bg-yellow-100" name="login" type="text" v-model="login" placeholder="Login...">
             <input class="focus:outline-yellow-500 focus:bg-yellow-100" name="password" type="password" v-model="password" placeholder="Password...">
         </div>
         <input class="submit-btn" :class="{active: isActiveBtn}" type="submit" :disabled="!isActiveBtn" :value="submitBtnTitle" @click="loginUser">
     </form>
-    <details v-if="error">
+    <details v-if="errorText">
         <summary class="text-red-500 font-medium">Error</summary>
-        <span>{{ error }}</span>
-        <ol>
+        <span>{{ errorText }}</span>
+        <ol v-if="rules">
             <li>The minimum login length is 3 characters.</li>
             <li>The password must contain from 6 to 20 characters, must contain at least one special character, at least one uppercase and lowercase letter.</li>
         </ol>
@@ -24,13 +25,17 @@ import { authServices } from "@/services/auth";
 import { useRouter } from "vue-router";
 import { useItems } from '@/utils/useItems';
 
+import Loader from '@/components/Loader.vue';
+
 const userData = auth();
 const { user, singIn } = storeToRefs(userData); 
 const { getFavorites } = useItems();
 
 const login = ref('');
 const password = ref('');
-const error = ref('');
+const errorText = ref('');
+const loading = ref(false);
+const rules = ref(false);
 
 const isActiveBtn = computed(() => login.value && password.value);
 const submitBtnTitle = computed(() => singIn.value ? 'Sing In' : 'Sing up');
@@ -48,22 +53,28 @@ function validateLogin(login) {
 };
 
 async function loginUser() {
-    try {
-        if(!singIn.value && (!validatePassword(password.value) || !validateLogin(login.value))) {
-            throw new Error('Read the rules!');
-        } else {
+    if(!singIn.value && (!validatePassword(password.value) || !validateLogin(login.value))) {
+        rules.value = true;
+        errorText.value = 'Read the rules!!!'
+    } else {
+        try {
+            loading.value = true;
             const params = {
             login: login.value,
             password: password.value,
             };
-            const { data } = singIn.value ? await authUser(params) : registerUser(params);
-            user.value.login = data.data.login;
-            user.value.id = data.data.id;
-            sessionStorage.setItem('user', JSON.stringify(user.value));
-            router.push('/');
+            const { data } = singIn.value ? await authUser(params) : await registerUser(params);
+            if (data) {
+                user.value.login = data?.data?.login;
+                user.value.id = data?.data?.id;
+                sessionStorage.setItem('user', JSON.stringify(user.value));
+                router.push('/');
+            }
+        } catch (error) {
+            errorText.value = error?.response?.data?.message;
+        } finally {
+            loading.value = false;
         }
-    } catch (err) {
-        error.value = err?.response?.data?.message ?? err;
     }
 };
 
